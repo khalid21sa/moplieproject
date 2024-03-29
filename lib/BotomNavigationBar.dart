a9e96125a99cea20312ae3d4ebb678265a499242
AlmontherAlhdhrami <116969342+AlmontherAlhdhrami@users.noreply.github.com>
2024-03-29 22:04:08 +0400

    update

---
+++ lib/BotomNavigationBar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  BottomNavBar({required this.selectedIndex, required this.onItemTapped});
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.blue[200]!,
+            Colors.blue[200]!,
+            Colors.blue[200]!,
+          ],
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, -1),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: widget.selectedIndex,
+        onTap: widget.onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 14.0,
+        unselectedFontSize: 12.0,
+        iconSize: 28.0,
+        backgroundColor: Colors.transparent,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Reservations',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
