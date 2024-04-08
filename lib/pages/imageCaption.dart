eaef949042f3b73ddef1ff4e685dbd9f4dd3efaa
L4ith <laythalramhi@gmail.com>
2024-04-08 18:57:36 +0400

    Laith

--- lib/pages/imageCaption.dart
+++ lib/pages/imageCaption.dart
@@ -13,8 +13,23 @@ class imageCaption extends StatelessWidget {
       alignment: Alignment.center,
       padding: EdgeInsets.all(0.8),
       child: Text(
-        caption
-      ),
+        caption,
+        textAlign: TextAlign.center, // Align the text in the center of the container
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.w600, // Set font weight to semi-bold
+          color: Colors.black,
+          letterSpacing: 0.5, // Add space between characters
+          height: 1.2, // Adjust line height
+          shadows: [ // Add text shadows
+            Shadow(
+              blurRadius: 2.0,
+              color: Colors.grey.withOpacity(0.5),
+              offset: Offset(2.0, 2.0),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
